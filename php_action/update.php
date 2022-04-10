4a1e5aa2bd9607a2779fec484bf21f44e0c5ef35
DevAlysonh <dev.alysonh@gmail.com>
2022-04-10 14:28:26 -0300

    Adiciona a função de proteção contra Cross site Scripting na aba editar

--- php_action/update.php
+++ php_action/update.php
@@ -2,13 +2,14 @@
 //sessão....
 session_start();
 //conexão db.....
+require_once 'add.php';
 require_once 'db_connect.php';
 //Atualizando os dados de clientes na tabela...
 if(isset($_POST['btn-editar'])):
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
     $id = mysqli_escape_string($connect, $_POST['id']);
     
         $sql = "UPDATE clientes SET nome='$nome', sobrenome='$sobrenome', email='$email', idade='$idade' WHERE id='$id'";
