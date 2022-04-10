ed5af0395ec726cbf550c4bd19580802f871599c
DevAlysonh <dev.alysonh@gmail.com>
2022-04-10 14:24:08 -0300

    Adiciona o metodo de segurança contra Cross Site Scripting

--- php_action/add.php
+++ php_action/add.php
@@ -2,12 +2,18 @@
 session_start();
 require_once 'db_connect.php';
 //cadastrando clientes na tabela....
+function clear($input){
+    global $connect;
+    $var = mysqli_escape_string($connect, $input);
+    $var = htmlspecialchars($input);
+    return $var;
+}
 
 if(isset($_POST['btn-cadastrar'])):
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
     
         $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
    
